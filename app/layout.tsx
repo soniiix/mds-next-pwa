f8aa373b7a3d2f7e7c47f3290386db4697326e4d
soniiix <quent.couzinet@gmail.com>
2025-10-02 15:26:38 +0200

    feat: include Inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
 import "./globals.css";
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+    subsets: ['latin'],
+})
 
 export default function RootLayout({
     children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+}: {
+    children: React.ReactNode
+}) {
     return (
-        <html lang="en">
-            <body>
-                {children}
-            </body>
+        <html lang="en" className={inter.className}>
+            <body>{children}</body>
         </html>
-    );
+    )
 }
