dd2ed9a15905c74c44cc52e349124dd1d22d9a20
soniiix <quent.couzinet@gmail.com>
2025-10-03 16:10:04 +0200

    feat: add github repo link to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import { GithubLogoIcon } from "@phosphor-icons/react";
+
 export default function Home() {
     return (
         <main className="flex-1 min-h-0 w-full flex flex-col items-center justify-center bg-neutral-50 text-neutral-900">
             <div className="m-auto flex items-center flex-col gap-4">
-                <h1 className="text-2xl font-bold">Application PWA avec Next.js.</h1>
+                <div className="flex items-center gap-2.5">
+                    <h1 className="text-2xl font-bold">Application PWA avec Next.js</h1>
+                    <a 
+                        className="text-orange-500 border border-orange-500 rounded-full p-1 transition-colors cursor-pointer"
+                        href="https://github.com/soniiix/mds-next-pwa"
+                        target="_blank" rel="noopener noreferrer"
+                    >
+                        <GithubLogoIcon size={22}/>
+                    </a>
+                </div>
                 <p className="text-center">
                     Les technologies utilisées dans cette application sont :&nbsp;
                     <a href="https://fr.react.dev/" className="text-orange-500 underline underline-offset-4" target="_blank" rel="noopener noreferrer">
