44195568b96701a7f253d70ed50374a9defdba4b
soniiix <quent.couzinet@gmail.com>
2025-10-23 14:46:36 +0200

    update

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -25,13 +25,15 @@ export default function Gallery() {
             <div className="px-6 py-4">
                 <div className="flex items-center justify-between mb-4">
                     <h2 className="text-lg font-bold">{photos.length} photos</h2>
-                    <button 
-                        onClick={() => clearGallery()} 
-                        className="px-4 py-2 flex items-center cursor-pointer bg-orange-100/90 text-orange-500 rounded-lg font-medium hover:bg-orange-100 transition"
-                    >
-                        Vider la galerie
-                        <TrashIcon size={18} className="ml-2" />
-                    </button>
+                    {photos.length > 0 && (
+                        <button 
+                            onClick={() => clearGallery()} 
+                            className="px-4 py-2 flex items-center cursor-pointer bg-orange-100/90 text-orange-500 rounded-lg font-medium hover:bg-orange-100 transition"
+                        >
+                            Vider la galerie
+                            <TrashIcon size={18} className="ml-2" />
+                        </button> 
+                    )}
                 </div>
                 <div className="grid grid-cols-5 gap-4">
                     {photos.map((photo) => (
